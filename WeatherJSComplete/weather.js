8fa988693f5b308d60d84b3107441a0a0280c1d9
Mitch Wilson <mitchwilson@blueyonder.co.uk>
2021-02-26 21:06:22 +0000

    2nd new

--- WeatherJSComplete/weather.js
+++ WeatherJSComplete/weather.js
@@ -7,7 +7,7 @@ class Weather{
     //Fetch weather from API
     async getWeather(){
         
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
         const responseData = await response.json();
        //console.log(responseData);
         return responseData;
