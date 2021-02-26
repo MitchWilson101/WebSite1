c61026b7e5b2a20a3fa2436425df85523cedc573
Mitch Wilson <mitchwilson@blueyonder.co.uk>
2021-02-26 20:41:45 +0000

    new start

--- WeatherJSComplete/weather.js
+++ WeatherJSComplete/weather.js
@@ -1,13 +1,13 @@
 class Weather{
     constructor(postCode){
- this.apiKey = '29c105619a5a457e9c2194357190712';
+ this.apiKey = '851bbfc86b714fdf98680509201201';
  this.city = postCode;
  
     }
     //Fetch weather from API
     async getWeather(){
         
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
         const responseData = await response.json();
        //console.log(responseData);
         return responseData;
